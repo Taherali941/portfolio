b4868e92ebcda914616bf7dea6015175e764c4f2
taher ali <salimshaikh@gmail.com>
2025-06-18 00:13:11 +0530

    fixed all errors and made it responsive and added hamburger menu and made some changes in about section

---
+++ script.js
@@ -0,0 +1,10 @@
+const checkbox = document.getElementById('menu-toggle')
+const hamnav = document.getElementById('ham-nav')
+
+checkbox.addEventListener('change',()=>{
+    if(checkbox.checked){
+        hamnav.style.display='block';
+    }else {
+        hamnav.style.display='none';
+    }
+})
